93898fcd3126669c35c85157973bd63aa2117984
dahaekim88 <dhkim1014@gmail.com>
2020-03-21 12:52:07 +0900

    Add handlers to update & delete task

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -47,4 +47,53 @@ module.exports = {
       throw error;
     }
   },
+  updateComplete: async (req, res) => {
+    try {
+      const { id, is_completed } = req.body;
+      const task = await db.Task.findByPk(id);
+      await task.update({
+        is_completed,
+      })
+      const tasks = await query.all();
+      res.json({
+        tasks,
+      });
+    } catch (err) {
+      const error = new VError("Failed to update the task complete.");
+      error.originalError = err;
+      throw error;
+    }
+  },
+  updateDuedate: async (req, res) => {
+    try {
+      const { id, due_date } = req.body;
+      const task = await db.Task.findByPk(id);
+      await task.update({
+        due_date,
+      })
+      const tasks = await query.all();
+      res.json({
+        tasks,
+      });
+    } catch (err) {
+      const error = new VError("Failed to save the due date.");
+      error.originalError = err;
+      throw error;
+    }
+  },
+  deleteTask: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const task = await db.Task.findByPk(id);
+      await task.destroy();
+      const tasks = await query.all();
+      res.json({
+        tasks,
+      });
+    } catch (err) {
+      const error = new VError("Failed to delete the task.");
+      error.originalError = err;
+      throw error;
+    }
+  },
 };
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,9 @@ const taskController = require("../controllers/tasks");
 
 router.get("/", taskController.getAllTasks);
 router.post("/", taskController.addNewTask);
+router.patch("/complete", taskController.updateComplete);
+router.patch("/duedate", taskController.updateDuedate);
 router.patch("/", taskController.updateTask);
+router.delete("/:id", taskController.deleteTask);
 
 module.exports = router;
